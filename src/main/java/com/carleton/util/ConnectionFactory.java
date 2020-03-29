9d9adcea159bcfb79d0aeb003c197e2a7d97ece7
Aayush Ranaut <aayush.ranaut@gmail.com>
2020-03-29 11:39:47 -0400

    Added: Migrated to SQLite

--- src/main/java/com/carleton/util/ConnectionFactory.java
+++ src/main/java/com/carleton/util/ConnectionFactory.java
@@ -8,10 +8,8 @@ public class ConnectionFactory {
 	// Static reference to itself
 	private static ConnectionFactory instance = new ConnectionFactory();
 
-	public static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
-	public static final String URL = "jdbc:mysql://localhost/dictionary?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	public static final String USER = "root";
-	public static final String PASSWORD = "mysqlpwd";
+	public static final String DRIVER_CLASS = "org.sqlite.JDBC";
+	public static final String URL = "jdbc:sqlite::resource:bug.db";
 	
 	// private constructor
 	private ConnectionFactory() {
@@ -25,7 +23,7 @@ public class ConnectionFactory {
 	private Connection createConnection() {
 		Connection connection = null;
 		try {
-			connection = DriverManager.getConnection(URL,USER,PASSWORD);
+			connection = DriverManager.getConnection(URL);
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("ERROR: Unable to Connect to Database.");
