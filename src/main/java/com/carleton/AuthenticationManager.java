77df894c62263dd783553f05fc4efa1a3ccf761d
Aayush Ranaut <aayush.ranaut@gmail.com>
2020-03-31 19:05:28 -0400

    Added code for Authentication Manager

--- src/main/java/com/carleton/AuthenticationManager.java
+++ src/main/java/com/carleton/AuthenticationManager.java
@@ -1,27 +1,49 @@
 package com.carleton;
 
 
-public class AuthenticationManager {
-	AuthenticationManager DB = new AuthenticationManager();
-	
-	public AuthenticationManager()
-	{
-		//setIconImage(Toolkit.getDefaultToolkit().getImage(System.getProperty("user.dir")+"/logo.png"));
-		
-	}
-	public static boolean varifyLogin(String username,String password, String role)
-	{
-		
-		boolean login = false; 
-		if(username.equals("admin") && password.equals("1234"))
-		{
-			//System.out.println(login);
-			login = true;
-			//System.out.println(login);
-		}
-		//System.out.println(login);
-		return login;
-		
-	}
+import com.carleton.util.ConnectionFactory;
+import com.carleton.util.DbUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class AuthenticationManager {
+    private static AuthenticationManager instance;
+    private Connection connection;
+
+    public int verifyLogin(String username, String password, String role) {
+        boolean login = false;
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            String sql = "SELECT * FROM bug_tracking_user WHERE username = ? AND password = ? and role = ? LIMIT 1";
+
+            PreparedStatement pstm = connection.prepareStatement(sql);
+
+            ResultSet rs = pstm.executeQuery();
+
+            while (rs.next()) {
+                int userId = rs.getInt("id");
+                return userId;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("SQLException in get() method");
+            e.printStackTrace();
+        } finally {
+            DbUtil.close(connection);
+        }
+
+        return 0;
+    }
+
+    public static AuthenticationManager getInstance() {
+        if (instance == null) {
+            instance = new AuthenticationManager();
+        }
+        return instance;
+    }
 }
